5c44975686a78dfb232b91f70329400b396baf7e
W-O-K <wojiakor@gmail.com>
2020-06-01 02:28:12 +0100

    Updates

---
+++ precache-manifest.b1f8c59fb28d5361d1e310b0f5550093.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb023c8345243650c29e61ffe8b66a63",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "e3ca2855fdfe100a12d0",
+    "url": "/todoapp/static/js/2.48c835f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.48c835f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb46a244470f4853e3fe",
+    "url": "/todoapp/static/js/main.aabc5f5d.chunk.js"
+  },
+  {
+    "revision": "6d95d2e9b93db28b3b5f",
+    "url": "/todoapp/static/js/runtime-main.d6958e2c.js"
+  }
+]);
